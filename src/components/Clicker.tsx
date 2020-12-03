ebdde2246e3f1ea5c1a666a4432080c3fd0aa20e
Ediii96 <47295918+Ediii96@users.noreply.github.com>
2020-12-03 18:51:40 +0300

    package

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -1,5 +1,13 @@
 namespace app {
 
-  export const Clicker = () => <button>123</button>
+  const {
+    TextField,
+  } = material.core;
+
+  const {
+
+  } = material.icons;
+
+  export const Clicker = () => <button>1234</button>
 
 } // namespace app
